34b0df41cab81f4e2480e5f6b6b7210ba7a3650f
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-25 06:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((sum, side) => sum + side);
+  }
+}
+
+class Triangle extends Polygon {
+  constructor() {
+    
+  }
+}
