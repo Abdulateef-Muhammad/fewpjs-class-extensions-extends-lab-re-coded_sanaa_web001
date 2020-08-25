3e25b2f903d7f9068928fe7f6a39ac2e30c77f8e
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-25 07:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class  Square extends Polygon {
   get isValid() {
     let sideLength = this.sides[0];
     console.log(sideLength);
-    return this.sides.includes(!!sideLength);
+    return this.sides.find( num => num !== sideLength);
   }
 
   get area() {
