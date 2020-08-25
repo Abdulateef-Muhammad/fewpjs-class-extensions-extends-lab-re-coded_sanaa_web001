5a27a81e111a571543d69b6de196edcb075fb711
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-25 07:57:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ class Triangle extends Polygon {
       let side3 = this.sides[2]
       console.log(side1, side2, side3)
       return
-        (side1 + side2 > side3) &&
-        (side2 + side3 > side1) &&
-        (side3 + side1 > side2);
+        (side1 + side2 > side3) 
+        //&&
+        // (side2 + side3 > side1) &&
+        // (side3 + side1 > side2);
     }
 }
 
